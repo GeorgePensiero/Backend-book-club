b00aa34bae81b57b26d10d1feaf0dc4e05d98f55
GeorgePensiero <georgepensiero@gmail.com>
2020-06-15 12:08:38 -0700

    create books table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_165621) do
+ActiveRecord::Schema.define(version: 2020_06_15_190436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "location", null: false
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_books_on_title", unique: true
+  end
 
   create_table "highlights", force: :cascade do |t|
     t.string "text", null: false
