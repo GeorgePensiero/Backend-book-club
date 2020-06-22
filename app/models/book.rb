75bf6d87f06964dca34bc2791c376fab46c63699
GeorgePensiero <georgepensiero@gmail.com>
2020-06-22 11:24:20 -0700

    book model fixing

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
-    validates: :title, :location, presence: true
-    validates: :title, uniqueness: true
+    validates :title, :location, presence: true
+    validates :title, uniqueness: true
 
     belongs_to :user,
-        foreign_key: :user_id
+        foreign_key: :user_id,
         class_name: :User
 
     has_many :highlights,
